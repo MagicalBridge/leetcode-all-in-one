494d1f29665c63a51b6b6a8c28fc565be3e689ee
褚鹏飞 <540788769@qq.com>
2020-06-12 09:33:22 +0800

    feat: add 添加206题目的题解

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-08 16:46:16
- * @LastEditTime: 2020-06-11 09:42:17
+ * @LastEditTime: 2020-06-12 09:30:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /leetcode/206.反转链表.js
@@ -41,26 +41,26 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-// 这个题目也是非常经典的一套题目，题目要求使用两种方式，一种是常规的解法。循环
-// 一种是使用递归的这种方法进行
-// 思路是首先创建一个空的节点，让头结点指向它，指向之前要注意，首先要将后继节点保存下来
-// 保存的目的是，当已经改变了head的next 执行null之后，这个时候取next 肯定就是null 了 而不是2 这个节点
-// 边界条件是 head 不是空的 且 链表 仅有一个节点的时候 不需要翻转
+// 链表翻转题目描述是将一个链表 翻转
+// 脑海中一定要有一个图谱 这个图谱是
 var reverseList = function (head) {
-  // 链表的相关问题一般都是可以通过递归或者普通遍历实现
-  if(head === null || head.next === null) {
-    return head; // 这是边界条件 如果本身是空，或者只有一个节点 无需翻转
-  }
-  let prev = null; // 设置一个虚拟头节点
-  let curr = head; // 将头结点的引用赋值给curr变量
+  // 设置一个头结点的前置节点;
+  let prev = null;
+  // 将头结点的赋值给curr curr 是不断向后面移动的
+  // 当curr 移动到 null 的时候循环就结束了
+  let curr = head;
 
-  while(curr) { // 是curr 不断的向后移动
-    let next = curr.next; // 将curr的next引用保存起来，防止做变换的时候引用错乱
-    curr.next = prev; // 将当前的节点next指针之前前置节点 null
+  while (curr) {
+    // 保存当前需要改变的头结点引用
+    let next = curr.next;
+    
+    // 开始进行变化操作，首先将curr.next 指向 prev
+    curr.next = prev;
 
+    // 然后将prev 移动到curr 然后将curr 移动到next
     prev = curr;
     curr = next;
   }
-  return prev;
+  return prev
 };
 
