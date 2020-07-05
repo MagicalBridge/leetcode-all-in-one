7276f06115d61f65ab476aea05d392977d81b973
褚鹏飞 <540788769@qq.com>
2020-07-05 17:15:53 +0800

    feat: 添加203道题目

---
+++ 203.移除链表元素.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=javascript
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+// 移除链表元素是常规操作之一，非常普通和简单
+var removeElements = function(head, val) {
+
+};
+// @lc code=end
+
