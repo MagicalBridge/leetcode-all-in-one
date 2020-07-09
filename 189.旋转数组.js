611a4e22ec58a8685439b34f8fae89ac4dd63e47
褚鹏飞 <540788769@qq.com>
2020-07-09 23:23:36 +0800

    feat: add 189 题目题解 使用第二种解法 中间变量

--- 189.旋转数组.js
+++ 189.旋转数组.js
@@ -16,11 +16,28 @@
 // i = 1 i 小于 3 符合条件 执行循环 数组变成了  [6,7,1,2,3,4,5]
 // i = 2 i 小于3 符合条件  执行循环 [5,6,7,1,2,3,4]
 // 但是使用这种方法比较low 但是能够解决问题。
+// var rotate = function (nums, k) {
+//   // 首先还是考虑边界条件
+//   for (let i = 0; i < k; i++) {
+//     nums.unshift(nums.pop());
+//   }
+// };
+
+// 解法2 
+// 使用的还是那种映射的关系 （i+k）% n --> i
+// 这样的解法还是需要一个中间数组来作为变量
 var rotate = function (nums, k) {
-  // 首先还是考虑边界条件
-  for (let i = 0; i < k; i++) {
-    nums.unshift(nums.pop());
+  let a = [];
+  for (let i = 0; i < nums.length; i++) {
+    console.log((i + k) % nums.length);
+    // console.log(i);
+    a[(i + k) % nums.length] = nums[i];
+  }
+  console.log(a)
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = a[i];
   }
 };
+rotate([1, 2, 3, 4, 5, 6, 7],3);
 // @lc code=end
 
