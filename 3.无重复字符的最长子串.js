a71d24f19653e948454a5d8bfe07d4da5cc2133e
褚鹏飞 <540788769@qq.com>
2020-07-10 10:07:00 +0800

    feat: add 无重复字符的最长子串

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -41,5 +41,33 @@ var lengthOfLongestSubstring = function (s) {
   }
   return max;
 };
-// @lc code=end
 
+// 在这里回忆一下解决方案，这个题目是没有重复的最长公共子串
+// 是维护一个数组 这个数组就是滑动窗口。
+
+// @lc code=end
+// 入参是一个字符串
+var lengthOfLongestSubstring = function (s) {
+  // 这道题目也没有边界条件，不过没有关系，边界条件可能没有
+  // 但是必须是需要思考的
+  let arr = [];// 设置一个空的数组作为滑动窗口
+  // 最终返回出去的是这个值。
+  let max = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    // 这个index 应该如取值呢？就是看看数组中是否有
+    // 这个对应的字符串 数组的indexOf 方法 查找每一个字符串
+    // 这个api的核心能力就是 在数组中查找这个字符串第一次出现的位置
+    // 如果查到了就返回第一个索引，如果没有查询到返回 -1;
+    let index = arr.indexOf(s[i]);
+
+    // 说明这个字符串在这个数组的内部，这个时候需要做的就是
+    // 将这个字符串和之前的元素全部清除。
+    if (index !== -1) {
+      arr.splice(0, index + 1);
+    }
+    // s.charAt(i) 返回的是 当前的元素 将元素push进去。
+    arr.push(s.charAt(i));
+    max = Math.max(arr.length, max)
+  }
+}
