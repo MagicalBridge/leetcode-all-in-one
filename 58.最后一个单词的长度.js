d48195c19d2de68080b20300e6d6db8a69d75bda
褚鹏飞 <540788769@qq.com>
2020-07-12 11:17:42 +0800

    feat: add 58 题 最后一个字符串的长度

--- 58.最后一个单词的长度.js
+++ 58.最后一个单词的长度.js
@@ -30,7 +30,25 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
+//
+var lengthOfLastWord = function (s) {
+  // end 是 字符串最后一位的下标，仔细思考为什么不是 length 而是 -1 操作
+  let end = s.length - 1;
 
+  // 循环遍历开始处理end 去除 尾部的空格。
+  while (end >= 0 && s[end] == ' ') {
+    // 假设最后一位是是空格且字符串的长度是 12 处理之后的end 就是10
+    end--
+  }
+  // 处理完空格之后，判断一下如果 ' ' end 就是 0 end -- 是 -1 说明本来就是空的
+  if (end < 0) {
+    return 0
+  }
+  let start =  end;
+
+  while(start >= 0  && s[start] !== ' ') {
+    start --;
+  }
+  return end - start;
 };
 
