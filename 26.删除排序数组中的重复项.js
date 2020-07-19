d23ab759ffd5df33851f1b312ada5ab1936769a4
褚鹏飞 <540788769@qq.com>
2020-07-19 18:10:05 +0800

    feat: add 提交26题目的新的解题方法

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -20,27 +20,6 @@
  * 
  * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
  * 
- * 你不需要考虑数组中超出新长度后面的元素。
- * 
- * 
- * 说明:
- * 
- * 为什么返回数值是整数，但输出的答案是数组呢?
- * 
- * 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
- * 
- * 你可以想象内部操作如下:
- * 
- * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
- * int len = removeDuplicates(nums);
- * 
- * // 在函数里修改输入数组对于调用者是可见的。
- * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
- * for (int i = 0; i < len; i++) {
- * print(nums[i]);
- * }
- * 
- * 
  */
 /**
  * @param {number[]} nums
@@ -64,17 +43,38 @@ var removeDuplicates = function (nums) {
       cur++
     }
   }
-  return pre+1;
+  return pre + 1;
 };
 
 // 这是第二种解法，根据js梁的说法，是另外一种的
 // 双指针思路。
-var removeDuplicates = function(nums) {
+var removeDuplicates = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === nums[i + 1]) {
       nums.splice(i, 1);
       i--;
     }
   }
+};
+
+// 数组完成排序后，我们可以放置两个指针 i 和 j 其中 i 是 慢指针，j 是快指针,
+// 只要nums[i] = nums[j] 我们就递增 j 跳过重复项。
+
+// 当我们遇到 nums[j] 不等于 nums[i] 的时候，跳过重复项的运行已经结束，因此我们
+// 必须把 nums[j]的值赋值给 nums[i+1]，然后递增i,接着我们将再次重复相同的过程，直到 jj 到达数组的末尾为止。
+
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
 };
 
