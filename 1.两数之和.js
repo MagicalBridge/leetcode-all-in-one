31725a5bd607756e62b5ee0d65eef9f6d0d37b6e
褚鹏飞 <540788769@qq.com>
2020-07-25 10:38:39 +0800

    feat: add 添加两数之和的另一种解法 只需要一层for循环

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -56,25 +56,76 @@
 // };
 
 //  0720复习两数之和
+// var twoSum = function (nums, target) {
+//   // 创建结果数组
+//   let resArr = [];
+//   // 创建一个map做数组的映射
+//   let map = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     map.set(nums[i], i);
+//   }
+//   console.log(map);
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let anotherNumber = target - nums[i];
+//     if (map.has(anotherNumber) && map.get(anotherNumber) !== i) {
+//       resArr.push(i);
+//       resArr.push(map.get(anotherNumber))
+//       break;
+//     }
+//   }
+//   return resArr;
+// }
+
+// 0725 复习两数之和 
+// 这道题目的要求是 给定一个数组和一个目标值，数组中的两个数的和等于目标值
+// 返回这两个索引
 var twoSum = function (nums, target) {
-  // 创建结果数组
-  let resArr = [];
-  // 创建一个map做数组的映射
+  // 边界条件: 数组的长度小于2 的时候已经不符合题目要求
+  if (nums.length < 2 || nums === undefined) {
+    return nums
+  }
+  let resArr = []; // 创建结果数组
+  // 唯手熟尔 使用map这种映射，映射的是 数字是啥和数字出现的位置
+  // 使用map 建立映射关系
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
+    // 创建映射关系
     map.set(nums[i], i);
   }
-  console.log(map);
 
+  // 第二次遍历, 每遍历一个数字就在map中寻找看是否存在
   for (let i = 0; i < nums.length; i++) {
-    let anotherNumber = target - nums[i];
+    // 另外一个数字，是目标的值减去当前的值
+    let anotherNumber = target - nums[i]
+    // 这个判断是 map中包含这个值,且不是当前这个值
     if (map.has(anotherNumber) && map.get(anotherNumber) !== i) {
+      // push 进去的是索引
       resArr.push(i);
-      resArr.push(map.get(anotherNumber))
+      resArr.push(map.get(anotherNumber));
       break;
     }
   }
   return resArr;
+}
+// 现在我们不仅要将代码写出来，还需要做的其实是，找到比较优秀的解法:
+// 这里面还有一个解法就是将两个for循环合并成一个
+// 这个题目开始的时候疑惑点是为什么没有
+var twoSum = function (nums, target) {
+  let map = new Map();
+  let resArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    // 因为是一次for循环 且map 中不能存在两个相同的key
+    // [2,3,2,5] target 是 4 返回的索引是 [0,2]
+    // 其实是一个边放数字边判断的过程
+    if (map.has(target - nums[i])) {
+      resArr[0] = i;
+      resArr[1] = map.get(target - nums[i])
+      break;
+    }
+    map.set(nums[i], i)
+  }
+  return resArr
 }
 
 // 测试用例
