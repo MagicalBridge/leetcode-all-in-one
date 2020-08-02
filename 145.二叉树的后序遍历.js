c518347a5f0056913520e53d97f257b4fc612a2e
褚鹏飞 <540788769@qq.com>
2020-08-02 12:35:01 +0800

    feat: add 添加后续遍历

---
+++ 145.二叉树的后序遍历.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=javascript
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 还是
+var postorderTraversal = function (root) {
+  let result = [];
+  helper(root, result);
+  return result;
+};
+// 
+var helper = function (root, result) {
+  if (root !== null) {
+    if (root.left !== null) {
+      helper(root.left, result)
+    }
+    if (root.right !== null) {
+      helper(root.right, result)
+    }
+    result.push(root.val)
+  }
+}
+// @lc code=end
+
