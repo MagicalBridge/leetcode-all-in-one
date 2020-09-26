e4452929c789b271e8857b9335c1ed68b161bcec
褚鹏飞 <540788769@qq.com>
2020-09-26 22:28:05 +0800

    lc104 二叉树的最大深度

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+
+};
+// @lc code=end
+
