41d96a2ae598488fde4e33c598bf1a9e181e7bd5
褚鹏飞 <540788769@qq.com>
2020-10-13 18:58:15 +0800

    feat: 数组扁平化实现

--- 数组扁平化/flatten.js
+++ 数组扁平化/flatten.js
@@ -1,17 +1,15 @@
-const flatten = (arr) => {
-  let result = [];
+function flatten(arr) {
+  let res = [];
 
   for (let i = 0; i < arr.length; i++) {
     if (Array.isArray(arr[i])) {
-      // 最近在实现这个个数组扁平化的过程中时候这部分总是写错
-      result = result.concat(flatten(arr[i]));
+      res = res.concat(flatten(arr[i]))
     } else {
-      result.push(arr[i])
+      res.push(arr[i])
     }
   }
-  return result;
+  return res;
 }
-
 const arr = [1, [2, 3, 4, 5, [7, 8, 9]]]
 const res = flatten(arr);
 console.log(res);
