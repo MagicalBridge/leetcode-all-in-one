a4fe9e060f1eb1b228e8f57aff6dc2e123fd28c5
褚鹏飞 <540788769@qq.com>
2020-10-23 20:35:59 +0800

    feat: add 添加bind的实现解析

--- 数组扁平化/flatten.js
+++ 数组扁平化/flatten.js
@@ -1,9 +1,12 @@
+/**
+ * 递归实现
+ * @param {*} arr 
+ */
 function flatten(arr) {
   let res = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      res = res.concat(flatten(arr[i]))
+  for (let i = 0, len = arr.length; i < len; i++) {
+    if (Array.isArray(arr[i])) { // 如果发现数组内部某一项还是数组 需要启动递归调用
+      res = res.concat(flatten(arr[i]));
     } else {
       res.push(arr[i])
     }
@@ -13,3 +16,4 @@ function flatten(arr) {
 const arr = [1, [2, 3, 4, 5, [7, 8, 9]]]
 const res = flatten(arr);
 console.log(res);
+
