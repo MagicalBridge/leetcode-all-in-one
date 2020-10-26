983b304fe23c7265cde85b549d2ba85f72755260
褚鹏飞 <540788769@qq.com>
2020-10-26 10:03:03 +0800

    feat: bind实现

--- bind的模拟实现/bind.js
+++ bind的模拟实现/bind.js
@@ -1,43 +1,18 @@
+// bind的模拟实现。bind函数 返回一个 函数 支持传递参数
+// 且返回的函数支持new 的方式调用
+// ES3中提供的方法 
 Function.prototype.bind2 = function (context) {
   var self = this;
-  // 获取bind2函数从第二个参数到最后一个参数
-  var args = Array.prototype.slice.call(arguments, 1);
-  console.log(args);
-  return function () {
-    // 这个时候的arguments是指bind返回的函数传入的参数
-    var bindArgs = Array.prototype.slice.call(arguments);
-    console.log(bindArgs)
-    // 分两次封装这个参数
-    return self.apply(context, args.concat(bindArgs));
+  // 处理参数，绑定的时候参数获取 从第一个参数往后截取
+  var bindArgs = Array.prototype.slice.call(arguments, 1);
+  var fBound = function () {
+    var args = Array.prototype.slice.call(arguments);
+    // 处理new 的指向问题
+    return self.apply(this instanceof fBound ? this : context, bindArgs.concat(args));
   }
+  fBound.prototype = this.prototype;
+  return fBound;
 }
-
-var value = 2;
-
-var foo = {
-  value: 1
-};
-
-function bar(name, age) {
-  this.habit = 'shopping';
-  console.log(this.value);
-  console.log(name);
-  console.log(age);
-}
-// 在bar原型上面添加了friend属性
-bar.prototype.friend = 'kevin';
-var bindFoo = bar.bind(foo, 'daisy');
-var obj = new bindFoo('18');
-// undefined
-// daisy
-// 18
-console.log(obj.habit);
-console.log(obj.friend);
-// shopping
-// kevin
-
-// 注意，尽管我们在foo中和全局中都申明了value 最后依然返回的是undefined说明绑定 this失效了
-// 这个时候this指向了obj
 
 // 第三版
 Function.prototype.bind2 = function (context) {
@@ -47,8 +22,7 @@ Function.prototype.bind2 = function (context) {
   var fBound = function () {
       var bindArgs = Array.prototype.slice.call(arguments);
       // 当作为构造函数时，this 指向实例，此时结果为 true，将绑定函数的 this 指向该实例，可以让实例获得来自绑定函数的值
-      // 以上面的是 demo 为例，如果改成 `this instanceof fBound ? null : context`，
-      // 实例只是一个空对象，将 null 改成 this ，实例会具有 habit 属性
+      // 以上面的是 demo 为例，如果改成 `this instanceof fBound ? null : context`，实例只是一个空对象，将 null 改成 this ，实例会具有 habit 属性
       // 当作为普通函数时，this 指向 window，此时结果为 false，将绑定函数的 this 指向 context
       return self.apply(this instanceof fBound ? this : context, args.concat(bindArgs));
   }
