275c9a37885b5a983fcd8bba987fd9185ffb6b6f
褚鹏飞 <540788769@qq.com>
2020-10-27 10:40:26 +0800

    feat: 节流的实现

--- 节流和防抖/节流/throttle.js
+++ 节流和防抖/节流/throttle.js
@@ -1,16 +1,35 @@
-// 节流的原理很简单 如果你持续触发事件，每隔一段时间 执行一次。
-// 节流的实现方式我这里使用定时器
-// 整体的实现和防抖类似。
-// 都是需要闭包来进行简单的实现。
+var count = 1;
+var container = document.getElementById('container');
+
+function getUserAction(e) {
+  container.innerHTML = count++;
+};
+
+container.onmousemove = throttle(getUserAction, 1000);
+
+// function throttle(func, wait) {
+//   var previous = 0;
+//   return function () {
+//     var now = +new Date();
+//     var context = this;
+//     var args = arguments;
+//     if (now - previous > wait) {
+//       func.apply(context, args);
+//       previous = now;
+//     }
+//   }
+// };
+
 function throttle(func, wait) {
-  var previous = 0;
-  return function () {
-    var now = +new Date();
-    var context = this;
-    var args = arguments;
+  let previous = 0;
+  return () => {
+    let args = arguments;
+    let context = this;
+    let now = +new Date();
     if (now - previous > wait) {
       func.apply(context, args);
       previous = now;
     }
   }
-};
+}
+
